c74c0946d63b8078fc02297607dd144fb91623e7
Flying-Droid <arusso2024@ash.nl>
2025-02-12 14:41:03 +0100

    Updated simple rover code
    This code has two functions as well as the start of code for the robot to turn

--- Tutorial code/Simple_RoverCode.py
+++ Tutorial code/Simple_RoverCode.py
@@ -11,7 +11,31 @@ servo_1 = servo.ContinuousServo(pwm)
 servo_2 = servo.ContinuousServo(pwm_2)
 
 
-while True:
+def straight():
+    .throttle = 0.5
+    m2.throttle = -0.45
+
+
+def straight():
+    servo_1.throttle = 0.365
+    servo_2.throttle = -0.5
+
+def stop():
+    servo_1.throttle = 0.0
+    servo_2.throttle = 0.0
+
+
+
+while true
+    print("right")
+    servo_1.throttle = 0
+    servo_2.throttle = 0.5
+    print("left")
+    servo_1.throttle = 0.5
+    servo_2.throttle = 0
+
+
+'''while True:
     print("forward")
     servo_1.throttle = 1.0
     servo_2.throttle = 1.0
@@ -28,3 +52,6 @@ while True:
     servo_1.throttle = -0.0
     servo_2.throttle = 0.0
     time.sleep(1.0)# Write your code here :-)
+
+
+
